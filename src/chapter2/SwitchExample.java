238509cd7ba43b7393c97e3454e6097f3b40da44
Chaean00 <jtkim965@naver.com>
2022-01-01 14:02:09 +0900

    6 commit

---
+++ src/chapter2/SwitchExample.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        int rank = 3;
+        char medalColor;
+
+        switch (rank) {
+            case 1:
+                medalColor = 'G';
+                break;
+            case 2:
+                medalColor = 'S';
+                break;
+            case 3:
+                medalColor = 'B';
+                break;
+            default:
+                medalColor = 'A';
+                break;
+        }
+        System.out.println(rank + "등 메달의 색깔은 " + medalColor + "입니다.");
+    }
+}
