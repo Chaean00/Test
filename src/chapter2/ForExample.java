eadb82899ff4b605b5625da62b95a0457022a294
Chaean00 <jtkim965@naver.com>
2022-01-01 18:21:29 +0900

    7 commit

---
+++ src/chapter2/ForExample.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+public class ForExample {
+    public static void main(String[] args) {
+        int dan = 2;
+        int times = 1;
+
+        while (dan<=9) {
+            if (dan % 2 == 1) {
+                dan++;
+                continue;
+            }
+            times = 1;
+            while (times<=9) {
+                System.out.println(dan + " X " + times + " = " + dan*times);
+                times++;
+            }
+            dan++;
+            System.out.println();
+        }
+    }
+}
---
+++ src/chapter2/ForExample2.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class ForExample2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, World");
+        }
+        System.out.println();
+
+        /* for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello, World");
+        } */
+    }
+}
